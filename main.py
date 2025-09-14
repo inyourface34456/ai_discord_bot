5ffc017483d851f58f89fe21a9ec15581c7eb77d
inyourface3445 <trenten46@gmail.com>
2025-09-13 20:15:21 -0400

    updated

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import os
 import webserver
+import requests
 
 DISCORD_TOKEN = os.environ.get("DISCORD_TOKEN")
 
@@ -17,6 +18,17 @@ async def on_ready():
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+
+@bot.command()
+async def prompt(ctx, *, text: str):
+    data = {
+        "prompt": f"{text}",
+        "max_tokens": 5000,
+    }
+
+    ctx.typing()
+    response = requests.post(f"https://storm-lowest-gd-receive.trycloudflare.com/v1/completions", json=data, headers={"Content-Type": "application/json"})
+    await ctx.send(response.json()["choices"][0]["text"])
 
 webserver.keep_alive()
 bot.run(DISCORD_TOKEN)
