f29da524bc6d91b2045ab07075404077ced30a27
inyourface3445 <trenten46@gmail.com>
2025-09-13 21:52:21 -0400

    update

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import discord
 from discord.ext import commands
-import os
 import webserver
-import requests
+import os
+import requests_async
 
 DISCORD_TOKEN = os.environ.get("DISCORD_TOKEN")
+MY_USER_ID = 804444072063664148
 
 intents = discord.Intents.all()
 # intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 url = ""
+length = 7000
 
 @bot.event
 async def on_ready():
@@ -24,19 +26,31 @@ async def ping(ctx):
 async def prompt(ctx, *, text: str):
     data = {
         "prompt": f"{text}",
-        "max_tokens": 1000,
+        "max_tokens": length,
     }
 
     await ctx.send("generating...")
-    response = requests.post(f"{url}/v1/completions", json=data, headers={"Content-Type": "application/json"})
-    await ctx.send(response.json()["choices"][0]["text"][::999])
+    response = await requests_async.post(f"{url}/v1/completions", json=data, headers={"Content-Type": "application/json"})
+    text = response.json()["choices"][0]["text"]
+    for chuck in [text[i:i + 2000] for i in range(0, len(text), 2000)]:
+        await ctx.send(chuck)
 
 @bot.command()
 async def set_url(ctx, loc_url: str):
     global url
-    if ctx.author.id == 804444072063664148:
+    if ctx.author.id == MY_USER_ID:
         url = loc_url
-        await ctx.send("Setting URL to {}".format(url))
+        await ctx.send(f"Set URL to {url}")
+    else:
+        await ctx.send("You are not authorized to use this command.")
+        return
+
+@bot.command()
+async def set_length(ctx, loc_length: int):
+    global length
+    if ctx.author.id == MY_USER_ID:
+        length = loc_length
+        await ctx.send(f"Set length to {length}")
     else:
         await ctx.send("You are not authorized to use this command.")
         return
